8e0d61afae968edb7f1a4a7f0f42881211e7e85a
Precious Emmanuel <preciousemmanuel@Preciouss-MacBook-Air.local>
2023-07-06 00:08:27 -0400

    terraform

---
+++ Desktop/Terraform practice/Variable.tf
@@ -0,0 +1,20 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "my_profile" {
+  default = "default"
+}
+variable "topic_name" {
+  default = "sns-dev"
+}
+variable "team" {
+  default = "dev"
+}
+variable "protocol_type" {
+  default = "email"
+}
+variable "end_point" {
+  default = "preciousigbokwe1819@gmail.com"
+}
